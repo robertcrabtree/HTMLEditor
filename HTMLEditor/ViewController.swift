d0bbff608767087661e12d5620fa6953fcadc605
Robert Crabtree <robert.crabtree@gmail.com>
2020-03-17 10:57:40 -0700

    Initial Commit

---
+++ HTMLEditor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HTMLEditor
+//
+//  Created by Rob Crabtree on 3/17/20.
+//  Copyright © 2020 Certified Organic Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
